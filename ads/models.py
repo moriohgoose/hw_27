4ea9507d4f7c0e17871ae4eacd354bc2389bff3e
moriohgoose <maria51m@mail.ru>
2023-05-12 03:04:39 +0300

    Initial commit

---
+++ ads/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Ad(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    address = models.CharField(max_length=200)
+    is_published = models.BooleanField()
+    price = models.PositiveIntegerField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    address = models.CharField(max_length=200)
+    is_published = models.BooleanField()
+    price = models.PositiveIntegerField()
+
---
+++ data/csv_to_json.py
@@ -0,0 +1,24 @@
+import csv
+import json
+
+
+def convert(csv_file, json_file, model):
+    result = []
+    with open(csv_file, encoding='utf-8') as f:
+        for row in csv.DictReader(f):
+            if 'is_published' in row:
+                if row['is_published'] == 'TRUE':
+                    row['is_published'] = True
+                else:
+                    row['is_published'] = False
+
+            result.append({"model": model, "fields": row})
+
+    with open(json_file, 'w', encoding='utf-8') as json_f:
+        json_f.write(json.dumps(result, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    convert('ads.csv', 'ads.json', 'ads.ad')
+    convert('categories.csv', 'categories.json', 'ads.category')
+
