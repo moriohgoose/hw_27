22278ff5e77cbff2c3bcd179982242973dcc5f2f
moriohgoose <maria51m@mail.ru>
2023-06-05 00:14:03 +0300

    1

---
+++ users/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    lat = models.DecimalField(max_digits=8, decimal_places=6, null=True, blank=True)
+    lon = models.DecimalField(max_digits=8, decimal_places=6, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Местоположение"
+        verbose_name_plural = "Местоположения"
+
+    def __str__(self):
+        return self.name
+
+class UserRoles(TextChoices):
+    MEMBER = "member", "Пользователь"
+    MODERATOR = "moderator", "Модератор"
+    ADMIN = "admin", "Админ"
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    role = models.CharField(max_length=10, choices=UserRoles.choices, default=UserRoles.MEMBER)
+    age = models.PositiveIntegerField()
+    locations = models.ManyToManyField(Location)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
